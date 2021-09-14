371fcc759309b0f281d2080bf3b06755fc7e782d
Agung Firmansyah <agungfir98@gmail.com>
2021-09-14 18:41:04 +0700

    Use express get to send Hello Express

--- myApp.js
+++ myApp.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var app = express();
 
-const absolutePath = ".views/index.html"
+
 app.get( "/", function (req, res) {
-    res.sendFile(absolutePath);
+    res.sendFile(__dirname + "/views/index.html");
 })
 console.log("Hello World");
 
