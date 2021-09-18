015a27a5d2668e0fe7240ceb4f0169bb9b83b38a
Agung Firmansyah <agungfir98@gmail.com>
2021-09-18 16:16:01 +0700

    (revised) implementing root-level request

--- myApp.js
+++ myApp.js
@@ -5,12 +5,10 @@ require("dotenv").config();
 
 app.use("/public", express.static(__dirname + "/public"))
 app.use( function middleware(req, res, next) {
-    console.log();
+    console.log(req.method + " + " + req.path + " - " + req.ip);
     app.get( "/", function (req, res) {
         res.sendFile(__dirname + "/views/index.html");
     })
-    console.log("Hello World");
-    
     app.get("/json", function (req, res) {
         if (process.env.MESSAGE_STYLE === "uppercase") {
             res.json({
